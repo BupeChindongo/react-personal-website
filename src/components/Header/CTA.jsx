7f78f08cfcf6dbf0fe25d768cfadd52abefa7046
U21566641 <u21566641@tuks.co.za>
2023-09-04 15:19:31 +0200

    CV added

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import cv from '../../assets/Bupe_Chindongo_CV.pdf'
 
 function CTA() {
     return (
         <div className="CTA">
-            <a href="#CV " className='btn'>Download CV</a>
+            <a href={cv} className='btn'>Download CV</a>
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
     )
