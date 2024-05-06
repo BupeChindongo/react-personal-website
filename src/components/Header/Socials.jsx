79e65751927493486a4c157a49e1b0ac4bec02e1
Bupe Chindongo <93218148+BupeChindongo@users.noreply.github.com>
2024-05-06 13:33:32 +0200

    Update Socials.jsx

--- src/components/Header/Socials.jsx
+++ src/components/Header/Socials.jsx
@@ -5,7 +5,7 @@ const Socials = () => {
     return (
         <div className='header__socials'>
             <a href='https://linkedin.com' target="_blank" rel="noopener noreferrer"><BsLinkedin /></a>
-            <a href='https://github.com/U21566641' target="_blank" rel="noopener noreferrer"><BsGithub /></a>
+            <a href='https://github.com/BupeChindongo' target="_blank" rel="noopener noreferrer"><BsGithub /></a>
             {/* <a href='https://linkedin.com' target="_blank"></a> */}
         </div>
     )
